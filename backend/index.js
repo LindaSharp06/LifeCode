40b04047e79663865efdeaea1b16aa350202b6c2
LindaSharp06 <linda0729sharp@outlook.com>
2026-03-24 21:22:03 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
